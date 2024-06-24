481cfc82a692e691acba8643be2f98c7042d31c2
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-24 01:17:30 -0700

    lists all records of the table second_table of the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- List all records of the table second_table from the database hbtn_0c_0
+-- This script retrieves all fields from the table second_table, displaying score and name, ordered by score in descending order
+
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
