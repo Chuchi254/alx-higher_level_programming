a94974155a33569357fc7317fd63942fb911e137
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-24 01:04:23 -0700

    displays the number of records with id = 89 in the table

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,6 @@
+-- Display the number of records with id = 89 in the table first_table
+-- This script counts the number of records with id = 89 in the table first_table and from the database hbtn_0c_0
+
+SELECT COUNT(*) AS record_count
+FROM first_table
+WHERE id = 98;
