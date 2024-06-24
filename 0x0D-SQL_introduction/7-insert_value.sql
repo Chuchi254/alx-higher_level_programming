9f6253382059aa9de08fee23fe60e4fa6485bc30
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-24 00:59:32 -0700

    inserts a new row in the given table and database

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Insert a new row into the table first_table in the database hbtn_0c_0
+-- This script inserts a row with id = 89 and name = 'Best School' into the table first_table
+
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
