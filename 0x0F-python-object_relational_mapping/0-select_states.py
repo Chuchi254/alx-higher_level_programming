1f9fd8ea380e65ce9e69e050a6e724323ddc96e8
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-24 05:11:08 -0700

    lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_0_usa.
+This script takes 3 arguments: mysql username, mysql password,
+and database name.
+It connects to a MySQL server running on localhost at port 3306
+and retrieves all states sorted by states.id in ascending order.
+"""
+
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get MySQL credentials from command line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    # Get MysQL credentials from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Connnect to the MySQL database
+    # Connect to the MySQL database
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
