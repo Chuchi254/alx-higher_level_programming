347bcd416bf7ff35caeec0813153a575daebb3c6
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-24 00:48:52 -0700

    prints full description of the a table from a given database

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Print the full description of the table first_table from the database hbtn_0c_0
+-- The script retrieves the column details of the table first_table
+
+SHOW CREATE TABLE first_table;
