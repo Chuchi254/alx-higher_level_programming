ead7d366e09e33c5d875bcd1e876d8dced21b5c5
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-23 23:54:37 -0700

    creates the database hbtn_0c_0 in your MySQL server

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Create the database hbtn_0c_0 if it does not already exist
+-- Creates the database hbtn_0c_0 in your MYSQL server, ensuring it doesn'tfail if the database already exists
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
