807d9863e62927bc86da1615037381b0feb4d684
Chuchi254 <samsongikonyo142@gmail.com>
2024-06-28 00:20:06 -0700

    prints ASCII values with e and q and without a new line

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) != 'e' and chr(i) != 'q':
-        print('{:c}'.format(i), end='')
+print("{}".format(
+    "".join(chr(i) for i in range(97, 123) if i not in (101, 113))), end="")
