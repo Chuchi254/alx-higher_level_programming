19fe73c63c2443d1987b3199264db2bce8d213dc
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-04 08:27:42 -0700

    update on file

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response for a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+
+curl -sL "$1"
