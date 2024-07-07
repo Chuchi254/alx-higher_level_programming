f4a6bbc7bb1a16a1fcf375d096b88e77d6264a84
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-07 04:59:23 -0700

    empty class square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 class Square:
-    """
-    An empty class that define a square.
+    """A class used to represent a Square.
+
+    This class does not have any attributes or methods.
+    It serves as a basic structure to define a square.
+    
+    Attributes:
+        None
     """
     pass
