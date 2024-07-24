cb36ec087f80d7126fe609ea9bc9176ce39299fc
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-24 08:33:39 -0700

    lists all shows in the database that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,3 @@
--- Use the specified database
-USE hbtn_0d_tvshows;
-
 -- List all shows with at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows, tv_show_genres
