4e3400ad37fb4aaf01340df520f850f2114f4386
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-30 11:47:28 -0700

    prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+  const fetchCharacterName = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          const character = JSON.parse(body);
+          resolve(character.name);
+        }
+      });
+    });
+  };
+
+  Promise.all(characters.map(fetchCharacterName))
+    .then((names) => {
+      names.forEach((name) => {
+        console.log(name);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
