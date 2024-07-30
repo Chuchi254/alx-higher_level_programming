631d61dfaaba070d7743ab171d40046cc798b078
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-30 12:14:43 -0700

    changes

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,19 +4,15 @@ const request = require('request');
 
 const apiUrl = process.argv[2];
 const characterId = '18';
-const baseUrl = apiUrl.endsWith('/') ? apiUrl : `${apiUrl}/`;
 
-request(baseUrl, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
 
-  try {
-    const films = JSON.parse(body).results;
-    const wedgeFilms = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
-    console.log(wedgeFilms.length);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
-  }
+  const films = JSON.parse(body).results;
+  const wedgeFilms = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+
+  console.log(wedgeFilms.length);
 });
