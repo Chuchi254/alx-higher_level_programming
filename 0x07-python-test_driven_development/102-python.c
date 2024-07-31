2cf430d68e5c3cbc43c8feec64ec03f04389e333
Chuchi254 <samsongikonyo142@gmail.com>
2024-07-31 01:39:18 -0700

    function that prints Python strings

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,38 @@
+#include <Python.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
+
+/**
+ * print_python_string - prints information about Python string
+ * @p: PyObject string object
+ */
+void print_python_string(pyObject *p)
+{
+	Py_ssize_t length;
+	wchar_t *value;
+
+	setlocale(LC_ALL, "");
+
+	printf("[.] string object info\n");
+
+	if (!PyUnicode_Check(p))
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = PyUnicode_GET_LENGTH(p);
+	value = PyUnicode_AsWideCharString(p, &length);
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", value);
+
+	PyMem_Free(value);
+}
